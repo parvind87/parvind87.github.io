e6734450c7b1e2f20422bb87d198f5c95569d59d
Parvind kumar <parvindkumar@Parvinds-MacBook-Pro.local>
2023-02-17 14:51:44 +0530

    News site

---
+++ news/precache-manifest.02bf01b74dd29094ceb0c954a35714ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb51fe17cfe1ec24f1b2a8c08e09026d",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "59b784de49288eb63e00",
+    "url": "/news/static/css/main.872f18c8.chunk.css"
+  },
+  {
+    "revision": "54bec344157a8ce812e1",
+    "url": "/news/static/js/2.eca15331.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/news/static/js/2.eca15331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b784de49288eb63e00",
+    "url": "/news/static/js/main.0fc1b47f.chunk.js"
+  },
+  {
+    "revision": "80b9b42d058a8dfc1bf1",
+    "url": "/news/static/js/runtime-main.c1bf3e50.js"
+  }
+]);
